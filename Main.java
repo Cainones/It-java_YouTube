b47349da630e5fc23976749306d8ebf86b41678b
kuistal <104359265+Cainones@users.noreply.github.com>
2022-12-10 14:28:11 +0300

    Java-YT

---
+++ Main.java
@@ -0,0 +1,28 @@
+package com.It-java_YouTube;
+
+public class Main {
+
+    public static void main (String[] args ){
+        // Comment: однострочный
+        /* Comment: многострочный */
+        System.out.println("Переменные и типы данных");
+
+        //int age = 18;
+            //age = 18;
+            //age = 26;
+        //System.out.printn("Возраст: "+ age +".");
+
+        //          'Числовые переменные'
+
+        //byte - от -128 до 127 место '1-байт'
+        //short - от -32768 до 32767 место '2-байт'
+        //int - от -2147483648 до 21474836447 место '4-байт'
+        //long - от -9 223 372 036 854 775 808 до 9 223 372 036 854 775 807'8-байт'
+
+        //float num4 = 4.656787f;
+        //double num5 = 6.4235345..; 'И.ТД' Для сверх точных вычислений нецелых чисел.
+
+        //booleen isHappy = false или true; Переменная может иметь только 2 значения false или true.
+    }
+
+}
